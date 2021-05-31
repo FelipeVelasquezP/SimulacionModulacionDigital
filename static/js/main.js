db775df27486d0b35ba5ad4615c1311bdd76f3c4
ACLXRD <andres.lopez01@correo.usa.edu.co>
2021-05-31 13:06:20 -0500

    Se crean cálculos de desviación de frecuencia, índice de modulacion, frecuencia de marca y espacio. Falta imprimirlos en la vista

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,20 @@ $("#btn-calcular").click(function() {
     const bit = parseFloat($("#bit"))
     const sensibilidad = parseFloat($("#sens"))
     if (amplitud && frecuencia && ascii && bit && sensibilidad) {
-        
+        // Cálculos uniformes
+        getDesviacion(sensibilidad, amplitud)
+        //Sesinsibilidad es un input
+        getfMarca(frecuencia, sensibilidad)
+        getfEspacio(frecuencia, sensibilidad)
+        getIndiceMod(getfMarca(frecuencia, sensibilidad),getfEspacio(frecuencia, sensibilidad),bit)
+        // Gráficas
+        pintarGraficaAscci(ascii,0.0016)
+        pintarGraficaPortadora(25, 5)
+        pintarEspectroFrecuencias(8, 25)
+        pintarGraficaSeñalModuladaASK(ascii,25,5,0.0016)
+        pintarGraficaSeñalModuladaFSK(ascii,0.0016,25,5,3.5)
+        pintarGraficaSeñalModuladaBPSK(ascii,0.0016,5)
+        pintarGraficaSeñalModuladaQPSK(ascii,5,2)
     } else {
         Swal.fire({
             icon: 'error',
@@ -17,10 +30,4 @@ $("#btn-calcular").click(function() {
     }
 })
 
-pintarGraficaAscci("e",0.0016)
-pintarGraficaPortadora(25, 5)
-pintarEspectroFrecuencias(8, 25)
-pintarGraficaSeñalModuladaASK("e",25,5,0.0016)
-pintarGraficaSeñalModuladaFSK("e",0.0016,25,5,3.5)
-pintarGraficaSeñalModuladaBPSK("e",0.0016,5)
-pintarGraficaSeñalModuladaQPSK("e",5,2)
+
